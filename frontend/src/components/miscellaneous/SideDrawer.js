8e6202191ca2b9c7b133dbd0f346435afd654301
Aditya Shah <adi.shah0912@gmail.com>
2022-04-20 17:05:46 +0530

    Added notification effect

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -9,6 +9,8 @@ import axios from 'axios';
 import ChatLoading from '../ChatLoading';
 import UserListItem from '../userAvatar/UserListItem';
 import { getSender } from '../../config/ChatLogics';
+import NotificationBadge from "react-notification-badge";
+import { Effect } from "react-notification-badge";
 
 const SideDrawer = () => {
 
@@ -131,6 +133,10 @@ const SideDrawer = () => {
             <div>
                 <Menu>
                     <MenuButton p={1}>
+                        <NotificationBadge
+                            count={notification.length}
+                            effect={Effect.SCALE}
+                        />
                         <BellIcon fontSize="2xl" m={1} />
                     </MenuButton>
                     <MenuList pl={2}>
